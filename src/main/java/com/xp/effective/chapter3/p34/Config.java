ac4b7b848ceb81a648f13cdd5c60d714dc94b14f
xiepeng <xpcoffee@163.com>
2017-09-04 15:16:37 +0800

    代理

--- src/main/java/com/xp/effective/chapter3/p34/Config.java
+++ src/main/java/com/xp/effective/chapter3/p34/Config.java
@@ -1,5 +1,7 @@
 package com.xp.effective.chapter3.p34;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Config {
 
-
+    @Bean
+    public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer(){
+        return new PropertyPlaceholderConfigurer();
+    }
 }
